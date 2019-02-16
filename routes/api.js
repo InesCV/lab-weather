94fcd25d93d20aa62d088c7b0c0cbe4508c2f107
Oriol Carbo <oriol.carbo@gmail.com>
2019-02-16 15:37:45 +0100

    Add Forecast in 5 days

--- routes/api.js
+++ routes/api.js
@@ -8,24 +8,36 @@ router.get('/', (req, res, next) => {
   res.render('index', { title: 'apiweather', copyright: '© 2019 JdeJ'});
 });
 
-router.post('/', (req, res, next) => {
+router.post('/', async (req, res, next) => {
   const { city } = req.body;
   const secret = 'b1ff600b1558ec3acda532913070f180';
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${secret}`;
+  
+  try {
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${secret}`;
+    const primera = await axios.get(url);
 
-  // Make a request for a user with a given ID
-  axios.get(url)
-    .then((response) => {
-      // handle success
-      const { data } = response;
-      console.log(response);
-      res.render('weather', { title: city, data });
-    })
-    .catch((error) => {
-      // handle error
-      console.log(error);
-      res.render('error', { title: 'Not found', city, error });
-    });
+    url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&APPID=${secret}`;
+    const segunda = await axios.get(url);
+
+    await res.render('weather', { title: city, primera, segunda });
+  } catch (error) {
+    console.log(error);
+    res.render('error', { title: 'Not found', city, error });
+  }
+
+  // // Make a request for a user with a given ID
+  // axios.get(url)
+  //   .then((response) => {
+  //     // handle success
+  //     const { data } = response;
+  //     console.log(response);
+  //     res.render('weather', { title: city, data });
+  //   })
+  //   .catch((error) => {
+  //     // handle error
+  //     console.log(error);
+  //     res.render('error', { title: 'Not found', city, error });
+  //   });
 });
 
 module.exports = router;
